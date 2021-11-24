5258cea562517248270093d31c63b04b1bab3c7c
Jhow Magnum Nascimento <jhokwmagnum@gmail.com>
2021-11-23 22:19:51 -0300

    Programa de soma simples

--- src/ComandosDeEntrada.java
+++ src/ComandosDeEntrada.java
@@ -13,31 +13,29 @@ public class ComandosDeEntrada {
 		Scanner entrada = new Scanner(System.in);
 		
 		/* Imprime toda frase por causa do nextLine()*/
-		System.out.printf("Informe uma frase: ");
-		vFrase = entrada.nextLine();
-		System.out.printf("A frase é: "+vFrase+space);
+		//System.out.printf("Informe uma frase: ");
+		//vFrase = entrada.nextLine();
+		//System.out.printf("A frase é: "+vFrase+space);
 		
 		/*Imprime somente uma palavra, por causa do next()*/
-		System.out.printf("Informe duas palavra: ");
-		vString = entrada.next();
-		
-		System.out.printf("A frase é: "+vFrase+space);
-		System.out.printf("A palavra é: "+vString+space);
+		//System.out.printf("Informe duas palavra: ");
+		//vString = entrada.next();
+		//System.out.printf("A palavra é: "+vString+space);
 		
 		/*Imprime valores Inteiro com a mudança do nextInt()*/
 		//System.out.printf("Informe um número inteiro: ");
 		//vInteiro = entrada.nextInt();
-		//System.out.printf("O número inteiro é: "+vInteiro);
+		//System.out.printf("O número inteiro é: "+vInteiro+space);
 		
 		/*Imprime Valores Double com a mudança do nextDouble()*/
 		//System.out.printf("Informe um Double: ");
 		//vDouble = entrada.nextDouble();
-		//System.out.printf("O Double é: "+vDouble);
+		//System.out.printf("O Double é: "+vDouble+space);
 		
 		/*Imprime Valores Float com a mudança do nextFloat()*/
 		//System.out.printf("Informe número Real: ");
 		//vFloat = entrada.nextFloat();
-		//System.out.printf("O número real é: "+vFloat);
+		//System.out.printf("O número real é: "+vFloat+space);
 		
 		
 	}
---
+++ src/ProgramaDeSomar.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ProgramaDeSomar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numA ;
+		int numB ;
+		
+		Scanner n1 = new Scanner(System.in);
+		Scanner n2 = new Scanner(System.in);
+		
+	 	System.out.printf("Informe um valor: ");
+		numA = n1.nextInt();
+		
+		
+		System.out.printf("Informe outro valor: ");
+		numB = n2.nextInt();
+		
+		int total = numA + numB;
+		System.out.println("A soma de "+numA+" e "+numB+" é: "+total);
+	}
+
+}
