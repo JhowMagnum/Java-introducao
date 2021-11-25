df024ac998d6e9c2c26687191afacfd4bdce10b3
Jhow Magnum Nascimento <jhokwmagnum@gmail.com>
2021-11-25 12:43:09 -0300

    IncrementoDecremento

--- src/IncrementoDecremento.java
+++ src/IncrementoDecremento.java
@@ -4,7 +4,38 @@ public class IncrementoDecremento {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 1;
+		int b = 1;
+		int c = 1;
+		int d = 1;
+		// Pós Incremento
+		System.out.println("Pós Decremento");
+		System.out.println(a++); // Incrementa mais 1 após a variavel
+		System.out.println(a+"\n"); //Resultado
+		
+		//Pré Incremento
+		System.out.println("Pré Incremento");
+		System.out.println(++b); // Incrementa mais 1 antes da variavel
+		System.out.println(b+"\n"); // Resultado
+		
+		/*================================================================*/
+		
+		//Pós Decremento
+		System.out.println("Pós Decremento");
+		System.out.println(c--); // Decrementa menos 1 após a variavel 
+		System.out.println(c+"\n"); // Resultado
+		
+		
+		//Pré Decremento
+		System.out.println("Pré Decremento");
+		System.out.println(--d); // Decrementa menos 1 antes da variavel
+		System.out.println(d+"\n"); //Resultado
+		
+		
+		
+		
 		
 	}
+	
+	
 
 }
